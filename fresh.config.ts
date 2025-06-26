acb2b3c0ca4dc5841e9104b9a41a638a028f2531
Fernando Frizzatti <fernandofrizzatti@gmail.com>
2025-06-26 11:45:18 -0300

    fix: fresh config

--- fresh.config.ts
+++ fresh.config.ts
@@ -1,11 +1,14 @@
 import { defineConfig } from "$fresh/server.ts";
 import { plugins } from "deco/plugins/deco.ts";
 import manifest from "./manifest.gen.ts";
+import { mcpServer } from "@deco/mcp";
 
 export default defineConfig({
   plugins: plugins({
     manifest,
-    // deno-lint-ignore no-explicit-any
-    
+    htmx: true,
+    useServer: (deco: any, hono) => {
+      hono.use("/*", mcpServer(deco));
+    },
   }),
 });
